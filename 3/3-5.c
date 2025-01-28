ae4a1690fb9b6ce7c2256dd666aed2da4e11dc8d
Jayden <hongkang@hongkang.name>
2025-01-28 00:54:09 -0500

    dev

---
+++ 3/3-5.c
@@ -0,0 +1,162 @@
+//
+// Created by Jayden Hong on 2025-01-27.
+//
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+// Fisher-Yates shuffle
+void shuffle_array(int *array, int size) {
+    for (int i = size - 1; i > 0; i--) {
+        int j = (int)(arc4random() % (i + 1));
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+void print_edges(unsigned char n, unsigned char f, bool cube) {
+    if(cube) {
+        printf("unused hypercube edge: ");
+    }
+
+    for (int i = 3; i >= 0; i--) {
+        int bit = (n >> i) & 1;
+        printf("%d", bit);
+    }
+    printf(" -> ");
+    for (int i = 3; i >= 0; i--) {
+        int bit = (f >> i) & 1;
+        printf("%d", bit);
+    }
+    printf("\n");
+}
+
+void flip(unsigned char root, int *dims, int size, unsigned char array[100][2], int *rows) {
+    shuffle_array(dims, size);
+
+    for(int i=0; i<size; i++) {
+        unsigned char f = root ^ (1 << dims[i]);
+        array[*rows][0] = root;
+        array[*rows][1] = f;
+        (*rows)++;
+
+        int k = size-i-1;
+        if(k > 0) {
+            int sub[k];
+            for (int j = 0; j < k; j++) {
+                sub[j] = dims[i+1+j];
+            }
+
+            flip(f, sub, k, array, rows);
+        }
+    }
+}
+
+void print_array(int *arr, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void cube(unsigned char root, int *dims, int size, unsigned char array[100][2], int *rows) {
+    // print_array(dims, size);
+
+    for(int i=0; i<size; i++) {
+        unsigned char f = root ^ (1 << dims[i]);
+
+        bool find = false;
+        for(int i=0;i<(*rows);i++) {
+            if(array[i][0] == root && array[i][1] == f) {
+                find = true;
+            }
+        }
+        if(!find) {
+            array[*rows][0] = root;
+            array[*rows][1] = f;
+            // print_edges(root, f, false);
+            (*rows)++;
+        }
+
+        int k = size - 1;
+        if(k > 0) {
+            int sub[k];
+            int m = 0;
+            for (int j = 0; j < size; j++) {
+                if(i != j) {
+                    sub[m] = dims[j];
+                    m++;
+                }
+            }
+
+            cube(f, sub, k, array, rows);
+        }
+    }
+}
+
+
+
+int main() {
+    // the dimensions of hypercube
+    int k = 4;
+    int dims[k];
+    for (int i = 0; i < k; i++) {
+        dims[i] = i;
+    }
+    // only work with the lower four bits of data
+    // you can change the root from 0b00000000 t0 0b00001111
+    unsigned char root = 0b00000110;
+
+    // to build a binomial tree started from one specific vertex
+    // we need to walk through all the vertices
+    // each time we can only flip one bit
+    unsigned char array[100][2];
+    int rows = 0;
+
+    flip(root, dims, k, array, &rows);
+
+    printf("edges of binomial tree: %d\n", rows);
+    for(int i=0;i<rows;i++) {
+        print_edges(array[i][0], array[i][1], false);
+    }
+    printf("\n");
+
+    // build hypercube
+    int cube_dims[k];
+    for (int i = 0; i < k; i++) {
+        cube_dims[i] = i;
+    }
+    unsigned char cube_root = 0b00000000;
+    unsigned char cube_array[100][2];
+    int cube_rows = 0;
+
+    cube(cube_root, cube_dims, k, cube_array, &cube_rows);
+
+    printf("edges of hypercube tree: %d\n", cube_rows);
+    for(int i=0;i<cube_rows;i++) {
+        print_edges(cube_array[i][0], cube_array[i][1], false);
+    }
+    printf("\n");
+
+    // compare
+    int total = 0;
+    for(int i=0;i<cube_rows;i++) {
+        bool find = false;
+        for(int j=0;j<rows;j++) {
+            if((cube_array[i][0] == array[j][0] && cube_array[i][1] == array[j][1]) ||
+            (cube_array[i][0] == array[j][1] && cube_array[i][1] == array[j][0])) {
+                find = true;
+            }
+        }
+        if(!find) {
+            print_edges(cube_array[i][0], cube_array[i][1], true);
+            total++;
+        }
+    }
+    printf("all %d unused\n", total);
+
+    return 0;
+}
---
+++ 3/3-5.h
@@ -0,0 +1,8 @@
+//
+// Created by Jayden Hong on 2025-01-27.
+//
+
+#ifndef PARALLEL_3_5_H
+#define PARALLEL_3_5_H
+
+#endif //PARALLEL_3_5_H
